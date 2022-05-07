cd2551d83972834eeed7485e07d5fe019dd7b507
Pedro Cunha <pdcunha02@gmail.com>
2022-05-07 15:01:59 +0100

    registo cliente

--- routes/login.js
+++ routes/login.js
@@ -23,29 +23,86 @@ router.get('/home', function (req, res, next) { // tentar integrar uma pagina ht
 router.get('/clienteslog', (req, res) => {
     execSQLQuery('SELECT * FROM Cllogin', res);
 })
+
+//registar cliente
 router.get('/registrarCL', function (req, res, next) { // tentar integrar uma pagina html
     res.render('registoCliente');
 })
-router.post('/registrarCL', function (req, res, next) {
-    console.log("request = " + JSON.stringify(req.body));
+
+router.post('/registrarCL', function (req, res, next) { //Pede os dados pessoais do cond e insere na bd
     var request = new sql.Request();
-    try {
-        request.input('CldataNasc', sql.Date, req.body.CldataNasc)
-        .input('Clnome', sql.VarChar(20), req.body.Clnome)
-        .input('ClnomeMeio', sql.VarChar(20), req.body.ClnomeMeio)
-        .input('Clapelido',sql.VarChar(20), req.body.Clapelido)
-        .input('Clgenero', sql.Char(1), req.body.Clgenero)
-        .input('ClTelemovel', sql.Int, req.body.ClTelemovel)
-        .query('INSERT INTO Cliente VALUES (@CldataNasc,@Clnome,@ClnomeMeio,@Clapelido,@Clgenero,@ClTelemovel)', function (err, result) {
-            console.log("result=" + JSON.stringify(result))
-            console.log("err=" + err)
-            res.redirect('/')
+    request.input('dataNasc', sql.Date, req.body.dataNasc)
+        .input('nome', sql.VarChar(20), req.body.nome)
+        .input('nomeMeio', sql.VarChar(20), req.body.nomeMeio)
+        .input('apelido',sql.VarChar(20), req.body.apelido)
+        .input('genero', sql.Char(1), req.body.genero)
+        .input('Telemovel', sql.Int, req.body.Telemovel)
+        .query('INSERT INTO Cliente VALUES (@dataNasc,@nome,@nomeMeio,@apelido,@genero,@Telemovel)', function (err, result) {
+            console.log("result"+JSON.stringify(result))
+            console.log("err"+JSON.stringify(err))
+
+            if(result.rowsAffected> 0){              
+                res.redirect('/registrarClLogin');
+            } else {
+                res.redirect('/registrarCL');
+            } 
         });
-    } catch (error) {
-        console.log("error=" + error)
-    }
-    
 });
+
+router.get('/registrarClLogin', function (req, res, next) { // Renderiza a pagina html
+    res.render('registoClLogin');
+})
+
+router.post('/registrarClLogin', function (req, res, next) { //Pede os dados de login do cond e insere na bd
+    var request = new sql.Request();
+    request.input('email', sql.VarChar(255), req.body.email)
+        .input('password', sql.VarChar(255), req.body.password)     
+        .query('INSERT INTO Cllogin VALUES (@email, @password,NULL)', function (err, result) {
+            console.log("result"+JSON.stringify(result))
+            console.log("err"+JSON.stringify(err))
+            if(result.rowsAffected> 0){              
+                res.redirect('/registrarClLogin2FA');
+            } else {
+                res.redirect('/registrarClLogin');
+            } 
+        });
+});
+
+router.get('/registrarClLogin2FA', function (req, res, next) { // Renderiza a pagina html
+    res.render('registoClLogin2FA');
+})
+
+router.post('/registrarClLogin2FA', function (req, res, next) { //Pede os dados pessoais do cond e insere na bd
+    var request = new sql.Request();                            //falta query
+    request.input('secret', sql.VarChar(20), req.body.secret)
+        .query('INSERT INTO Conlogin VALUES (@email, @password)', function (err, result) {
+            console.log("result"+JSON.stringify(result))
+            console.log("err"+JSON.stringify(err))
+            res.redirect('/adicionarMetPag')
+        });
+});
+
+router.get('/adicionarMetPag', function (req, res, next) { // Renderiza a pagina html
+    res.render('addMetPag');
+})
+
+router.post('/adicionarMetPag', function (req, res, next) { //Pede os dados da carta condução
+    var request = new sql.Request();
+    request.input('ano', sql.Int, req.body.ano)
+        .input('mes', sql.Int, req.body.mes)
+        .input('numCartao', sql.VarChar(15), req.body.mes)
+        .input('CVV', sql.Int, req.body.mes)
+        .query('INSERT INTO CartaConducao VALUES (@ano,@mes,@numCartao,@CVV)', function (err, result) {
+            console.log("result"+JSON.stringify(result))
+            console.log("err"+JSON.stringify(err))
+            if(result.rowsAffected> 0){              
+                res.redirect('/adicionarMetPag');
+            } else {
+                res.redirect('/loginCl');
+            } 
+        });
+});
+
 //registrar condutor
 router.get('/registrarCon', function (req, res, next) { // Renderiza a pagina html
     res.render('registoCond');
